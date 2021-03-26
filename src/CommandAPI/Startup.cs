8ea8e8868245e95bc08a4094758a55c6d9091732
Torres Francisco <francisco.tadeu.torres@gmail.com>
2021-03-26 19:06:30 +0000

    First Migration

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -6,16 +6,31 @@ using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        //added by me
+        //This is Configuration Code. We'll use to supply the connection strign in 'appsettings.json' to
+        //our DbContext class.
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Added by me
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+                (Configuration.GetConnectionString("PostgreSqlConnection")));
+
             //Added by me
             services.AddControllers();
 
